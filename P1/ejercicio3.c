416fe21ae129376c4677feb0f2e029a9529f171b
ahcode <alvaro.hpg4@gmail.com>
2016-03-05 12:04:24 +0100

    Función log e3 y modificación .gitignore

--- P1/ejercicio3.c
+++ P1/ejercicio3.c
@@ -8,56 +8,73 @@
 
 #define MAX_SIZE 5
 
-int addLog(char * cad);
+void funcionLog(char *mensaje);
+FILE *fLog = NULL;
 
 int main(){
 	int fildes[2], i, flag, status;
 	pid_t pid;
-	char buf[MAX_SIZE];
+	char buf[MAX_SIZE], out[300];
 
 	if (pipe(fildes)==-1){
-		printf("Error al crear la tubería\n");
+		sprintf(out,"Error al crear la tubería");
+		printf("%s\n",out);
+		funcionLog(out);
 		return 1;
 	}
 
 	switch(pid=fork()){
 		case -1:
-			printf("Error en la creación del proceso hijo\n");
+			sprintf(out,"Error en la creación del proceso hijo");
+			printf("%s\n",out);
+			funcionLog(out);
 			return 1;
 
 		case 0:
-			printf("[HIJO] mi PID es %d y mi PPID es %d\n",getpid(),getppid());
+			sprintf(out,"[HIJO] mi PID es %d y mi PPID es %d",getpid(),getppid());
+			printf("%s\n",out);
+			funcionLog(out);
 			if (close(fildes[1])==-1)
 					perror("Error al cerrar la tubería");
 
 			for(i=0;i<5;i++){
 				if(read(fildes[0],buf,MAX_SIZE)==-1)
 					perror("Error en la escritura en la tubería");
-				printf("[HIJO]: leemos el número aleatorio %s de la tubería\n",buf);
+				sprintf(out,"[HIJO] leemos el número aleatorio %s de la tubería",buf);
+				printf("%s\n",out);
+				funcionLog(out);
 			}
 
 			if (close(fildes[0])==-1)
 					perror("Error al cerrar la tubería");
-			printf("[HIJO]: tubería cerrada. Salgo...\n");
+			sprintf(out,"[HIJO] tubería cerrada. Salgo...");
+			printf("%s\n",out);
+			funcionLog(out);
 			return 0;
 
 		default:
-			printf("[PADRE] mi PID es %d y el PID de mi hijo es %d\n",getpid(),pid);
+			sprintf(out,"[PADRE] mi PID es %d y el PID de mi hijo es %d",getpid(),pid);
+			printf("%s\n",out);
+			funcionLog(out);
 			srand(time(NULL));
 			if (close(fildes[0])==-1)
 				perror("Error al cerrar la tubería");
 
 			for(i=0;i<5;i++){
 				sprintf(buf,"%d",rand()%5000);
+				sprintf(out,"[PADRE] escribimos el número aleatorio %s en la tubería",buf);
+				printf("%s\n",out);
+				funcionLog(out);
 				if(write(fildes[1],buf,MAX_SIZE)==-1)
 					perror("Error en la lectura de la tubería");
-				printf("[PADRE] escribimos el número aleatorio %s en la tubería\n",buf);
 				usleep(rand()%1000000);
 			}
 
 			if (close(fildes[1])==-1)
 					perror("Error al cerrar la tubería");
-			printf("[PADRE]: tubería cerrada\n");
+			sprintf(out,"[PADRE] tubería cerrada");
+			printf("%s\n",out);
+			funcionLog(out);
 			break;
 	}
 
@@ -65,25 +82,72 @@ int main(){
 	{
 		if (WIFEXITED(status))
 		{
-			printf("Hijo PID:%d finalizado, estado=%d\n", flag, WEXITSTATUS(status));
+			sprintf(out,"Hijo PID:%d finalizado, estado=%d", flag, WEXITSTATUS(status));
+			printf("%s\n",out);
+			funcionLog(out);
 		} else if (WIFSIGNALED(status)) {  //Para seniales como las de finalizar o matar
-			printf("Hijo  PID:%d finalizado al recibir la señal %d\n", flag, WTERMSIG(status));
+			sprintf(out,"Hijo  PID:%d finalizado al recibir la señal %d", flag, WTERMSIG(status));
+			printf("%s\n",out);
+			funcionLog(out);
 		} else if (WIFSTOPPED(status)) { //Para cuando se para un proceso. Al usar wait() en vez de waitpid() no nos sirve.
-			printf("Hijo PID:%d parado al recibir la señal %d\n", flag,WSTOPSIG(status));
+			sprintf(out,"Hijo PID:%d parado al recibir la señal %d", flag,WSTOPSIG(status));
+			printf("%s\n",out);
+			funcionLog(out);
 		} else if (WIFCONTINUED(status)){ //Para cuando se reanuda un proceso parado. Al usar wait() en vez de waitpid() no nos sirve.
-			printf("Hijo reanudado\n");
+			sprintf(out,"Hijo reanudado");
+			printf("%s\n",out);
+			funcionLog(out);
 		}
 	}
 	if (flag==-1 && errno==ECHILD)
 	{
-		printf("No hay más hijos que esperar\n");
-		printf("status de errno=%d, definido como %s\n", errno, strerror(errno));
+		sprintf(out,"No hay más hijos que esperar");
+		printf("%s\n",out);
+		funcionLog(out);
+		sprintf(out,"status de errno=%d, definido como %s", errno, strerror(errno));
+		printf("%s\n",out);
+		funcionLog(out);
 	}
 	else
 	{
-		printf("Error en la invocacion de wait o la llamada ha sido interrumpida por una señal.\n");
+		sprintf(out,"Error en la invocacion de wait o la llamada ha sido interrumpida por una señal.");
+		printf("%s\n",out);
+		funcionLog(out);
 		exit(EXIT_FAILURE);
 	}
 
 	return 0;
+}
+
+void funcionLog(char *mensaje){
+	int resultado;
+	char nombreFichero[100];
+	char mensajeAEscribir[300];
+	time_t t;
+
+	// Abrir el fichero
+	sprintf(nombreFichero,"ejercicio3.txt");
+	if(fLog==NULL){
+		fLog = fopen(nombreFichero,"at");
+		if(fLog==NULL){
+			perror("Error abriendo el fichero de log");
+			exit(1);
+		}
+	}
+
+	// Obtener la hora actual
+	t = time(NULL);
+	struct tm * p = localtime(&t);
+	strftime(mensajeAEscribir, 1000, "[%Y-%m-%d, %H:%M:%S]", p);
+
+	// Vamos a incluir la hora y el mensaje que nos pasan
+	sprintf(mensajeAEscribir, "%s ==> %s\n", mensajeAEscribir, mensaje);
+
+	// Escribir finalmente en el fichero
+	resultado = fputs(mensajeAEscribir,fLog);
+	if ( resultado < 0)
+		perror("Error escribiendo en el fichero de log");
+
+	fclose(fLog);
+	fLog=NULL;
 }
