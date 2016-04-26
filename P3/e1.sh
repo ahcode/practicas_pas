479958aff506a9ab399de05b1b941fd3f7ab8b98
ahcode <alvaro.hpg4@gmail.com>
2016-04-26 14:04:32 +0200

    P3. Terminado e1 y e2

--- P3/e1.sh
+++ P3/e1.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
-f="series.txt"
 echo "****************************"
 echo "1) Títulos de las series:"
-grep -E '[0-9]+\. ' $f
+grep -E '[0-9]+\. ' $1
 echo "****************************"
 echo "2) Cadenas que producen las series:"
-grep '^\* .* \*$' $f
+grep '^\* .* \*$' $1
 echo "****************************"
 echo "3) Cadenas que producen las series sin asteriscos ni espacios:"
-grep '^\* .* \*' $f | grep -o '[[:alpha:]]*'
+grep '^\* .* \*' $1 | grep -o '[[:alpha:]]*'
 echo "****************************"
 echo "4) Eliminar las líneas de sinopsis:"
-grep -v 'SINOPSIS:' $f
+grep -v 'SINOPSIS:' $1
 echo "****************************"
 echo "5) Eliminar líneas vacías:"
-grep -v '^$' $f
+grep -v '^$' $1
 echo "****************************"
 echo "6) Contar cuantas series produce cada cadena:"
-for x in $(grep '^\* .* \*' $f | grep -o '[[:alpha:]]*' | sort -u)
+for x in $(grep '^\* .* \*' $1 | grep -o '[[:alpha:]]*' | sort -u)
 do
-  n=$(grep -c $x $f)
+  n=$(grep -c $x $1)
   echo "La cadena $x produce $n series"
 done
 echo "****************************"
 echo "7) Líneas que contengan una palabra en mayúsculas entre paréntesis:"
-grep -E '\([[:upper:]].*\)' $f
+grep -E '\([[:upper:]].*\)' $1
 echo "****************************"
 echo "8) Emparejamientos de palabras repetidas en la misma línea:"
-grep -o -E '\b([[:alpha:]]+)\b.*\b\1\b' $f
+grep -o -E '\b([[:alpha:]]+)\b.*\b\1\b' $1
+echo "****************************"
+echo "9) Líneas que contienen 28 aes o más:"
+grep -E '([aA].*){28,}' $1
+echo "10) Nombre de película y temporadas:"
+grep -E '^=+$' -C 1 $1
