b36f6e762bba9473d5cafbbd4ee0bf3257e1c943
ahcode <alvaro.hpg4@gmail.com>
2016-05-08 12:16:10 +0200

    P3. fin e5

--- P3/e5.sh
+++ P3/e5.sh
@@ -5,3 +5,5 @@ echo -n "Número de hilos máximo en ejecución: "
 grep -c '^model name' /proc/cpuinfo
 echo "Puntos de montaje: "
 cat /proc/mounts | sort -k 3 | sed -r -e 's/^(.*) (.*) (.*) .* .* .*/-> Punto de montaje: \2, Dispositivo: \1, Tipo de dispositivo: \3/'
+echo "Particiones y número de bloques: "
+grep -E '^ +[0-9]' /proc/partitions | sort -k 3 -n | sed -r -e 's/^ +[0-9]+ +[0-9]+ +([0-9]+) +(.*)/-> Partición: \2, Número Bloques: \1/'
