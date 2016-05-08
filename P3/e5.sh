38666f3e4efdd197d05b2ad42bfa39bd39c1b257
ahcode <alvaro.hpg4@gmail.com>
2016-05-08 12:02:39 +0200

    P3. Más de e5

--- P3/e5.sh
+++ P3/e5.sh
@@ -4,3 +4,4 @@ grep -m 1 '^cpu MHz' /proc/cpuinfo | sed -r -e 's/.*: (.*)/Megahercios: \1/'
 echo -n "Número de hilos máximo en ejecución: "
 grep -c '^model name' /proc/cpuinfo
 echo "Puntos de montaje: "
+cat /proc/mounts | sort -k 3 | sed -r -e 's/^(.*) (.*) (.*) .* .* .*/-> Punto de montaje: \2, Dispositivo: \1, Tipo de dispositivo: \3/'
